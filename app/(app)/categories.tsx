07f17ff470754e7412d32922bdd3fd2155e73094
hihhhello <grigori.ratushniak@gmail.com>
2024-02-23 17:50:54 -0600

    add categories list

--- app/(app)/categories.tsx
+++ app/(app)/categories.tsx
@@ -1,7 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
-import { View } from 'react-native';
+import { Stack } from 'expo-router';
+import { FlatList, Pressable, RefreshControl, View } from 'react-native';
 
 import { api } from '@/shared/api/api';
+import { SquaresPlusIcon } from '@/shared/icons/SquaresPlusIcon';
+import { COLORS } from '@/shared/theme';
 import { Text } from '@/shared/ui/Text';
 
 export default function CategoriesScreen() {
@@ -11,8 +14,63 @@ export default function CategoriesScreen() {
   });
 
   return (
-    <View>
-      <Text>Categories</Text>
+    <View style={{ flex: 1 }}>
+      <Stack.Screen
+        options={{
+          title: 'Categories',
+        }}
+      />
+
+      <FlatList
+        data={categoriesQuery.data}
+        refreshControl={
+          <RefreshControl
+            refreshing={categoriesQuery.isFetching}
+            onRefresh={categoriesQuery.refetch}
+          />
+        }
+        columnWrapperStyle={{
+          justifyContent: 'space-between',
+        }}
+        numColumns={3}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <Pressable>
+            <View
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                gap: 8,
+                paddingBottom: 16,
+              }}
+            >
+              <View
+                style={[
+                  {
+                    height: 64,
+                    width: 64,
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    borderRadius: 100,
+                    backgroundColor: COLORS.main.paper,
+                  },
+                ]}
+              >
+                <SquaresPlusIcon width={32} height={32} color="#000" />
+              </View>
+
+              <Text
+                numberOfLines={1}
+                style={{ width: 80, textAlign: 'center' }}
+              >
+                {item.name}
+              </Text>
+            </View>
+          </Pressable>
+        )}
+      />
     </View>
   );
 }
