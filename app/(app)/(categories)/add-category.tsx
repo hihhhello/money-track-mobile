862bc2ddee4af9c0da16e1d6d9003f1dabd21592
hihhhello <grigori.ratushniak@gmail.com>
2024-03-01 18:08:49 -0600

    add categories create api and refetch on screen focus

--- app/(app)/(categories)/add-category.tsx
+++ app/(app)/(categories)/add-category.tsx
@@ -1,11 +1,42 @@
+import { useMutation } from '@tanstack/react-query';
+import { useLocalSearchParams, useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
+import { useState } from 'react';
+import { Button, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
+import { api } from '@/shared/api/api';
+import { FinancialOperationTypeValue } from '@/shared/types/globalTypes';
 import { TextInput } from '@/shared/ui/TextInput';
 
 export default function AddCategoryModal() {
+  const router = useRouter();
+  const searchParams = useLocalSearchParams<{
+    categoryType: FinancialOperationTypeValue;
+  }>();
   const insets = useSafeAreaInsets();
+  const [categoryName, setCategoryName] = useState('');
+
+  const { mutate: apiCreateCategory, ...createCategoryMutation } = useMutation({
+    mutationFn: api.categories.createOne,
+    mutationKey: ['api.categories.createOne'],
+    onSuccess: () => {
+      router.back();
+    },
+  });
+
+  const handleAddCategory = () => {
+    if (!searchParams.categoryType) {
+      return;
+    }
+
+    apiCreateCategory({
+      body: {
+        name: categoryName,
+        type: searchParams.categoryType,
+      },
+    });
+  };
 
   return (
     <View
@@ -14,7 +45,18 @@ export default function AddCategoryModal() {
         paddingTop: insets.top,
       }}
     >
-      <TextInput placeholder="Category name" />
+      <TextInput
+        placeholder="Category name"
+        value={categoryName}
+        onChangeText={setCategoryName}
+      />
+
+      <Button
+        title={
+          createCategoryMutation.isPending ? 'Loading...' : 'Add new category'
+        }
+        onPress={handleAddCategory}
+      />
 
       <StatusBar style="light" />
     </View>
--- app/(app)/(categories)/categories.tsx
+++ app/(app)/(categories)/categories.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
-import { Link } from 'expo-router';
+import { Link, useNavigation } from 'expo-router';
+import { useCallback, useEffect, useMemo } from 'react';
 import {
   Button,
   FlatList,
@@ -11,70 +12,191 @@ import {
 import { api } from '@/shared/api/api';
 import { SquaresPlusIcon } from '@/shared/icons/SquaresPlusIcon';
 import { COLORS } from '@/shared/theme';
+import { Category } from '@/shared/types/categoryTypes';
+import { FinancialOperationType } from '@/shared/types/globalTypes';
 import { Text } from '@/shared/ui/Text';
 
 export default function CategoriesScreen() {
+  const navigation = useNavigation();
+
   const categoriesQuery = useQuery({
     queryFn: api.categories.getAll,
     queryKey: ['api.categories.getAll'],
   });
 
+  const refetchCategories = useCallback(() => {
+    categoriesQuery.refetch();
+  }, [categoriesQuery]);
+
+  useEffect(() => {
+    navigation.addListener('focus', refetchCategories);
+
+    return () => {
+      navigation.removeListener('focus', refetchCategories);
+    };
+  }, [categoriesQuery, navigation, refetchCategories]);
+
+  const reducedCategories = useMemo(
+    () =>
+      categoriesQuery.data?.reduce<{
+        deposit: Category[];
+        expense: Category[];
+      }>(
+        (categoriesAccumulator, category) => {
+          if (category.type === FinancialOperationType.DEPOSIT) {
+            return {
+              ...categoriesAccumulator,
+              deposit: [...categoriesAccumulator.deposit, category],
+            };
+          } else
+            return {
+              ...categoriesAccumulator,
+              expense: [...categoriesAccumulator.expense, category],
+            };
+        },
+        {
+          deposit: [],
+          expense: [],
+        },
+      ),
+    [categoriesQuery.data],
+  );
+
   return (
-    <View style={{ flex: 1 }}>
-      <FlatList
-        data={categoriesQuery.data}
-        refreshControl={
-          <RefreshControl
-            refreshing={categoriesQuery.isFetching}
-            onRefresh={categoriesQuery.refetch}
-          />
-        }
-        columnWrapperStyle={{
-          justifyContent: 'space-between',
-        }}
-        numColumns={3}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <Pressable>
-            <View
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                gap: 8,
-                paddingBottom: 16,
-              }}
-            >
+    <View style={{ flex: 1, gap: 16 }}>
+      <View style={{ backgroundColor: COLORS.main.paper, flex: 1 }}>
+        <Text>Expenses</Text>
+
+        <FlatList
+          data={reducedCategories?.expense}
+          refreshControl={
+            <RefreshControl
+              refreshing={categoriesQuery.isFetching}
+              onRefresh={categoriesQuery.refetch}
+            />
+          }
+          columnWrapperStyle={{
+            justifyContent: 'space-between',
+          }}
+          numColumns={3}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <Pressable>
               <View
-                style={[
-                  {
-                    height: 64,
-                    width: 64,
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    borderRadius: 100,
-                    backgroundColor: COLORS.main.paper,
-                  },
-                ]}
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  gap: 8,
+                  paddingBottom: 16,
+                }}
               >
-                <SquaresPlusIcon width={32} height={32} color="#000" />
+                <View
+                  style={[
+                    {
+                      height: 64,
+                      width: 64,
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      borderRadius: 100,
+                      backgroundColor: '#fff',
+                    },
+                  ]}
+                >
+                  <SquaresPlusIcon width={32} height={32} color="#000" />
+                </View>
+
+                <Text
+                  numberOfLines={1}
+                  style={{ width: 80, textAlign: 'center' }}
+                >
+                  {item.name}
+                </Text>
               </View>
+            </Pressable>
+          )}
+        />
 
-              <Text
-                numberOfLines={1}
-                style={{ width: 80, textAlign: 'center' }}
+        <Link
+          href={{
+            pathname: 'add-category',
+            params: {
+              categoryType: FinancialOperationType.EXPENSE,
+            },
+          }}
+          asChild
+        >
+          <Button title="Add new category" />
+        </Link>
+      </View>
+
+      <View style={{ backgroundColor: COLORS.main.paper, flex: 1 }}>
+        <Text>Deposits</Text>
+
+        <FlatList
+          data={reducedCategories?.deposit}
+          refreshControl={
+            <RefreshControl
+              refreshing={categoriesQuery.isFetching}
+              onRefresh={categoriesQuery.refetch}
+            />
+          }
+          columnWrapperStyle={{
+            justifyContent: 'space-between',
+          }}
+          numColumns={3}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <Pressable>
+              <View
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  gap: 8,
+                  paddingBottom: 16,
+                }}
               >
-                {item.name}
-              </Text>
-            </View>
-          </Pressable>
-        )}
-      />
+                <View
+                  style={[
+                    {
+                      height: 64,
+                      width: 64,
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      borderRadius: 100,
+                      backgroundColor: '#fff',
+                    },
+                  ]}
+                >
+                  <SquaresPlusIcon width={32} height={32} color="#000" />
+                </View>
 
-      <Link href="add-category" asChild>
-        <Button title="Add new category" />
-      </Link>
+                <Text
+                  numberOfLines={1}
+                  style={{ width: 80, textAlign: 'center' }}
+                >
+                  {item.name}
+                </Text>
+              </View>
+            </Pressable>
+          )}
+        />
+
+        <Link
+          href={{
+            pathname: 'add-category',
+            params: {
+              categoryType: FinancialOperationType.DEPOSIT,
+            },
+          }}
+          asChild
+        >
+          <Button title="Add new category" />
+        </Link>
+      </View>
     </View>
   );
 }
